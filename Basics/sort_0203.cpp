8cf97fe7ef45aaebb7d5a750430b117ec8693146
nana7812 <zcjswkz0812@naver.com>
2021-02-03 22:24:35 +0900

    가장 큰 수

---
+++ Basics/sort_0203.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(string a, string b){
+    return a+b > b+a;
+}
+
+string solution(vector<int> numbers) {
+    vector<string> v;
+    for(int i = 0; i < numbers.size(); i++){
+        v.push_back(to_string(numbers[i]));
+    }
+    sort(v.begin(),v.end(),compare);
+    string answer = "";
+    for(int i = 0; i <v.size(); i++){
+        answer += v[i];
+    }
+    if(answer[0] == '0') return "0";
+
+    return answer;
+}
