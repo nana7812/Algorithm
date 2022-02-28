c270f1a69d559c7000904548bcd0fb9e167c6faf
nana7812 <zcjswkz0812@naver.com>
2022-02-28 15:34:34 +0900

    시험감독

---
+++ SWTEST/so1_0228.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+long long N,B,C;
+long long a[1000005];
+
+int main(){
+    cin>> N;
+    for(long long i = 0; i < N; i++){
+        cin>>a[i];
+    }
+    cin>> B>> C;
+    long long answer = N;
+    for(long long i = 0; i < N; i++){
+        a[i] -= B;
+        if(a[i] > 0){
+           answer += (a[i]/C);
+           if(a[i] % C != 0){
+              answer += 1;
+           } 
+        }
+    }
+    cout<<answer<<"\n";
+    return 0;
+}
