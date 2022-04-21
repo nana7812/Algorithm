db03df31803ae0ef72b760b86398ab08efc5c29a
nana7812 <zcjswkz0812@naver.com>
2022-04-21 23:39:32 +0900

    사다리 조작

---
+++ SWTEST/sol5_0421.cpp
@@ -0,0 +1,62 @@
+#include <vector>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+int N, M,H;
+int answer;
+bool ladder[12][32];
+
+bool check() {
+    for (int i = 1; i <= N; i++) {
+        int cur = i;
+        for (int j = 1; j <= H; j++) {
+            if (ladder[cur][j]) cur = cur + 1;
+            else if (ladder[cur - 1][j]) cur = cur - 1;
+        }
+        if (cur != i) return false;
+    }
+    return true;
+}
+
+void select(int idx, int cnt){
+
+    if(cnt >= 4) return;
+
+    if(check()){
+        if(answer == -1 || answer > cnt){
+            answer = cnt;
+            return;
+        }
+    }
+
+    for(int i = idx; i <= H; i++){
+        for(int j = 1; j < N; j++){
+            if(ladder[j][i]) continue;
+            if(ladder[j-1][i]) continue;
+            if(ladder[j+1][i]) continue;
+
+            if(answer == -1 || cnt+1 < answer){
+                ladder[j][i] = true;
+                select(i,cnt+1);
+                ladder[j][i] = false;
+            }
+        }
+    }
+}
+
+int main() {
+    cin>>N>> M>> H;
+    for(int i = 0; i < M; i++){
+        int a,b;
+        cin>>a >> b;
+        ladder[b][a] = true;
+    }
+
+    answer = -1;
+    select(1,0);
+
+    cout<<answer<<"\n";
+    return 0;
+}
